a325e76e834ebb5faa124c04969637b33e0b21ea
gatere <mercynjerigatere@gmail.com>
2024-05-27 12:07:09 +0300

    Adds lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    sequence.append(0)
+    sequence.append(1)
+    
+    while len(sequence) < length :
+        next_number = sequence[(len(sequence) - 1)] + sequence[(len(sequence) - 2)]
+        sequence.append(next_number)
+    return sequence[0:length]
+    
+print(print_fibonacci(9))
